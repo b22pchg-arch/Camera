3c57d0763cfa86a84999c3c9454414012ba3f12a
b22pchg-arch <b22pchg@gmail.com>
2026-05-19 08:00:05 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,9 +11,12 @@ const ASSETS = [
   './manifest.json',
   './icon-192.png',
   './icon-512.png',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
-  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
+  'qrcode.min.js',
+  'bootstrap.min.css',
+  'all.min.css',
+  'bootstrap.bundle.min.js',
+    'webfonts/fa-solid-900.woff2',  // <-- Khóa cứng file font icon hay dùng dưới hiện trường
+    'webfonts/fa-regular-400.woff2' // <-- Khóa cứng file font icon hay dùng dưới hiện trường
 ];
 
 // Cài đặt và tải tài nguyên
