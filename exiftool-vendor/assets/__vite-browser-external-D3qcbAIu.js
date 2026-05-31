feb82275656f6720691852f8a7858815c11e07c8
b22pchg-arch <b22pchg@gmail.com>
2026-05-31 15:14:05 +0700

    Create __vite-browser-external-D3qcbAIu.js

---
+++ exiftool-vendor/assets/__vite-browser-external-D3qcbAIu.js
@@ -0,0 +1,7 @@
+import { t as e } from "../exiftool-browser.es.js";
+//#region __vite-browser-external
+var t = /* @__PURE__ */ e(((e, t) => {
+	t.exports = {};
+}));
+//#endregion
+export default t();
